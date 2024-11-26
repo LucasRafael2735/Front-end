d6592f142a4fce02b1cad0a620f8d760e87e1907
LucasRafael2735 <lucasgithub27@gmail.com>
2024-11-26 11:46:01 -0300

    Add files via upload

---
+++ aula14/testes/script.js
@@ -0,0 +1,34 @@
+/*1. Click Event (click)
+Description: Triggered when the user clicks on an element, such as a button, link, or div.
+Example:
+javascript
+
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+});
+Usage: Commonly used for buttons, links, and interactive elements to handle user interactions.
+2. Keydown Event (keydown)
+Description: Triggered when a user presses a key on the keyboard.
+Example:
+javascript
+
+document.addEventListener("keydown", function(event) {
+    console.log("Key pressed: " + event.key);
+});
+Usage: Often used to capture keyboard inputs for games, search bars, or shortcut keys.*/
+
+
+/* function displayNone() {
+    return document.getElementById(display).innerHTML ="Lucas"
+}
+
+function styleText() {
+    return document.getElementById("hello").style.fontSize = "50px"
+}
+
+function mudaTamanho() {
+    x =  document.getElementById("troca").style.backgroundcolor = "purple"
+    y =  document.getElementById("troca").style.width = "1000px"
+    z =  document.getElementById("troca").style.height = "1000px"
+} */
+
